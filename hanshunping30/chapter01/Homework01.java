5c4f4cb2594d56189e009a6525197a79af00b5e9
StrudyGrass <2804597126@qq.com>
2024-12-05 19:58:28 +0800

    把hanshunping30文件夹都提交上去

---
+++ hanshunping30/chapter01/Homework01.java
@@ -0,0 +1,12 @@
+public class Homework01{
+	public static void main(String[] args){
+		System.out.println("hello world~");
+	}
+}
+
+
+class Homework02{
+	public static void main(String[] args){
+		System.out.println("Name\tSex\tJiguan\tAdress\nwangyulei\tboy\thebei\tbeijing");
+	}
+}
