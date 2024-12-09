81db37a4122dab0ef3bb184b15d531074227bdc3
StrudyGrass <2804597126@qq.com>
2024-12-09 15:02:59 +0800

    二维数组+杨辉三角 在公司电脑的dev分支上，推到github

---
+++ hanshunping30/chapter06/YanghuiTriangle.java
@@ -0,0 +1,36 @@
+public class YanghuiTriangle {
+	public static void main(String[] args){
+		int [][] arr = new int [10][];
+		int molecule = 1 ;  //fenzi
+		int denominator = 1 ;  //fenmu
+		int iclone ;
+		for (int i = 0 ; i < arr.length ; i++){
+			arr[i] = new int [i+1];
+			arr[i][0] = 1 ;
+			for (int j = 1 ; j < arr[i].length ; j++){
+				// jisuan : fenmu
+				for (int m = j ; m >= 1 ; m-- ){
+					denominator *= m ;
+				}
+				// jisuan : fenzi 
+				iclone = i ;
+				for (int m = 1 ; m <=j  ; m++ ){					
+					molecule *= iclone ;
+					iclone-- ;
+				}				
+ 				arr[i][j] = molecule / denominator ;
+				
+				molecule = 1 ;
+				denominator = 1 ; 
+
+			}
+		}
+		System.out.println("------------");
+		for (int i = 0 ; i < arr.length ; i++){
+			for (int j = 0 ; j < arr[i].length ; j++){
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
