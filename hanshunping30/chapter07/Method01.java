0589fd479d5284743fc9a28fdb6b643e5b749219
StrudyGrass <2804597126@qq.com>
2024-12-11 14:13:21 +0800

    公司电脑，在dev分支上，提交一些方法入门的东西，推到github上

--- hanshunping30/chapter07/Method01.java
+++ hanshunping30/chapter07/Method01.java
@@ -2,6 +2,10 @@ public class Method01 {
 	public static void main(String[] args){
 		Person p = new Person();
 		p.speak();
+		System.out.println(p.calc01(1000));
+		System.out.println(p.getSum(1 , 2 ));
+		
+		
 
 	}
 }
@@ -11,7 +15,19 @@ class Person{
 	int age ;
 
 	public void speak(){
-		System.out.println("i am a nice Person");
+		System.out.println("I am a nice Person");
+	}
+
+	public int calc01(int n){
+		int sum = 0 ; 
+		for (int i = 1 ; i <= n ; i++){
+			sum += i ;
+		}
+		return sum ;
+	}
+
+	public double getSum(double m ,double n){
+		return (m + n );
 	}
 
 
