68a963699f9ad2eb4f723314f0436f481f8c745a
NorthFish <w18230097876@163.com>
2024-12-16 23:24:13 +0800

    家里电脑 dev分支上，做的作业提交一下 并推到github

--- hanshunping30/chapter07/HomeWork.java
+++ hanshunping30/chapter07/HomeWork.java
@@ -26,6 +26,38 @@ public class HomeWork {
 		System.out.println("after updatePrince , the price is " + h3_5.updatePrince());
 
 
+		System.out.println("************************");
+		System.out.println("HomeWork08") ;
+		new HomeWork08().count1(); //10
+		HomeWork08 h8 = new HomeWork08() ;
+		h8.count2() ;//9
+		h8.count2() ;//10
+
+
+		System.out.println("************************");
+		System.out.println("HomeWork10") ;
+		HomeWork10 h10_1 = new HomeWork10();
+		HomeWork10 h10_2 = h10_1 ;
+		h10_2.m(); // i = 101 j = 100 
+		System.out.println(h10_1.i); // 101
+		System.out.println(h10_2.i); // 101
+
+		System.out.println("************************");
+		System.out.println("HomeWork11") ;
+		HomeWork11 h11 = new HomeWork11();
+		System.out.println(h11.method(h11.method(10.0 , 20.0) , 100));
+
+		System.out.println("************************");
+		System.out.println("HomeWork12") ;
+		HomeWork12 h12_1 = new HomeWork12 ("zahngsan" , 'W' , 24);
+		h12_1.show() ;
+		HomeWork12 h12_2 = new HomeWork12 ("boss" , 50000.12 );
+		h12_2.show() ;
+		HomeWork12 h12_3 = new HomeWork12 ("lina" , 'M' , 32 , "taxi driver" , 18546.78 );
+		h12_3.show() ;
+
+
+
 
 	}
 }
@@ -58,5 +90,155 @@ class HomeWork03 {
 	public double updatePrince(){
 		this.price = this.price > 100 ? ( this.price > 150 ? 150 : 100 ) : this.price ;
 		return this.price ;
+	}
+}
+
+class HomeWork04 {
+	public int[] copyArr (int[] arr){
+		int [] copyArr = new int[arr.length] ;
+		for (int i = 0 ; i < arr.length ; i++){
+			copyArr[i] = arr[i] ;
+		}
+		return copyArr ;
+	}
+
+	public double[] copyArr (double[] arr){
+		double [] copyArr = new double[arr.length] ;
+		for (int i = 0 ; i < arr.length ; i++){
+			copyArr[i] = arr[i] ;
+		}
+		return copyArr ;
+	}
+
+	public String[] copyArr (String[] arr){
+		String [] copyArr = new String[arr.length] ;
+		for (int i = 0 ; i < arr.length ; i++){
+			copyArr[i] = arr[i] ;
+		}
+		return copyArr ;
+	}
+
+}
+
+class HomeWork05 {	
+	double radius ;
+	public HomeWork05 (double radius){
+		this.radius = radius ;
+	}
+	public double perimeter(double radius){
+		return 2 * 3.14159265354 * radius ;
+	}
+	public double area(double radius){
+		return 3.14159265354 * radius * radius ;
+	}
+}
+
+class HomeWork06 {	
+	double num1  , num2 ;
+	public double add (double num1 , double num2){
+		return num1 + num2 ;
+	}
+	public double subtract(double num1 , double num2){
+		return num1 - num2 ;
+	}
+	public double multiply(double num1 , double num2){
+		return num1 * num2 ;
+	}
+	public double divide(double num1 , double num2){
+		if(num2 == 0 ){
+			System.out.println("error");
+			return 0 ;
+		}else{
+			return num1 / num2 ;			
+		}
+	}
+}
+
+class HomeWork07 {	
+	String name ;
+	String color ;
+	int age ;
+	public HomeWork07 (String name , String color , int age){
+		this.name = name ;
+		this.color = color ;
+		this.age = age ;
+	}
+	public void show (){
+		System.out.println("this.name : " + this.name ) ;
+		System.out.println("this.color : " + this.color ) ;
+		System.out.println("this.age : " + this.age ) ;
+	}
+}
+
+class HomeWork08 {	
+	int count = 9 ;
+	public void count1 (){
+		count = 10 ;
+		System.out.println("count1 = " + count ) ;
+	}
+	public void count2 (){
+		System.out.println("count1 = " + count++ ) ;
+	}
+}
+
+class HomeWork09 {	
+	String name ;
+	double times ;
+	public void play (){
+		System.out.println("this.name : " + this.name ) ;
+		System.out.println("this.times : " + this.times ) ;
+		System.out.println("123456789") ;
+	}
+	
+	public void getInfo (){
+		System.out.println("this.name : " + this.name ) ;
+		System.out.println("this.times : " + this.times ) ;
+	}
+}
+
+class HomeWork10 {	
+	int i = 100 ;
+	public void m (){
+		int j = i++ ;
+		System.out.println("i = " + i ) ;
+		System.out.println("j = " + j ) ;
+	}	
+}
+
+class HomeWork11 {
+	public double method (double n1 , double n2){
+		return n1 + n2 ;
+	}
+}
+
+class HomeWork12 {
+	String name ;
+	char sex ;
+	int age ;
+	String position ;
+	double salary ;
+	public HomeWork12 (String name , char sex , int age ){
+		this.name = name ;
+		this.sex = sex ;
+		this.age = age ;
+	}
+	public HomeWork12 (String position , double salary ){
+		this.position = position ;
+		this.salary = salary ;
+	}
+	public HomeWork12 (String name , char sex , int age , String position , double salary ){
+		this( name , sex , age ) ;
+		this.position = position ;
+		this.salary = salary ;
+	}
+	public void show (){
+		System.out.println("--------------------" );
+		System.out.println("name :" + this.name );
+		System.out.println("sex :" + this.sex );
+		System.out.println("age :" + this.age );
+		System.out.println("position :" + this.position );
+		System.out.println("salary :" + this.salary );
+		System.out.println("--------------------" );
+
 	}
 }
