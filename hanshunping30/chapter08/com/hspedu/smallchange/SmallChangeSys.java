b0469503df488e0d1729ef8403d83a0bcfaa4bc5
NorthFish <w18230097876@163.com>
2024-12-30 23:03:20 +0800

    家里电脑dev分支上，修改了面向过程的零钱通的代码，写了面向对象的零钱通，然后自己写了本章第一个作业，提交一下并推到github

--- hanshunping30/chapter08/com/hspedu/smallchange/SmallChangeSys.java
+++ hanshunping30/chapter08/com/hspedu/smallchange/SmallChangeSys.java
@@ -41,8 +41,11 @@ public class SmallChangeSys {
         String note = "";//定义变量记录消费说明
 
         //5、改进：退出增加退出确认
-        String verify = "";
+        String choice = "";
         boolean exitLoop = true;
+
+        //6、改进：对收益入账和消费支出的额度增加合理性校验
+
 
         do {
             System.out.println("\n=============零钱通菜单=============");
@@ -63,6 +66,13 @@ public class SmallChangeSys {
                 case "2": {
                     System.out.println("请输入收益入账金额：");
                     money = scanner.nextDouble();//先不做校验，之后再加
+                    //找出不正确的金额条件，然后给出提示，只直接break
+                    if (money <= 0) {
+                        System.out.println("收益入账金额不正确");
+                        break;
+                    }
+                    //之后这里可以写其他不正常的情况
+                    //...
                     date = new Date();//获取系统时间作为收益入账的时间
                     banlance += money; //更新总余额
                     details += "\n收益入账\t+" + money + "\t" + sdf.format(date) + "\t余额：" + banlance;//更新零钱通明细总字符串
@@ -74,6 +84,10 @@ public class SmallChangeSys {
                     note = scanner.next();//note从scanner那获取消费说明
                     System.out.println("请输入消费额度：");
                     money = scanner.nextDouble();//money从scanner那获取消费额度
+                    if (money <= 0 || money > banlance) {//筛掉输入的消费额度不正确的情况
+                        System.out.println("消费额度不正确");
+                        break;
+                    }
                     date = new Date();//获取系统时间作为消费的时间
                     banlance -= money;//更新总余额
                     details += "\n" + note + "\t-" + money + "\t" + sdf.format(date) + "\t余额：" + banlance;//更新零钱通明细总字符串
@@ -85,6 +99,7 @@ public class SmallChangeSys {
                     //选择退出时，增加确认提示：你确定要退出吗？y/n
                     //进入退出确认的循环
                     //必须输入正确的y/n，否则循环输入指令
+                    /*
                     exitLoop = true;//每次选4都要进入退出确认的循环
                     while (exitLoop) {
                         System.out.println("你确定要退出吗？y/n");
@@ -106,6 +121,23 @@ public class SmallChangeSys {
                         }
                     }
                     break;
+                    */
+                    //按照老韩的思路再解耦合一下
+                    //循环只确认是不是输入的y/n
+                    //一段代码只完成一件小事
+                    while (true) {
+                        System.out.println("你确定要退出吗？y/n");
+                        choice = scanner.next();
+                        if ("y".equals(choice) || "n".equals(choice)) {
+                            break;
+                        }
+                    }
+                    //能出来循环说明肯定选了y/n
+                    if ("y".equals(choice)) {
+                        loop = false;
+                    }
+                    break;
+
                 }
                 default: {
                     System.out.println("选择有误，请重新选择");
